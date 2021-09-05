7872a4a1a9812d4763ad88677c154df71cb12a6c
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-09-05 00:16:54 -0300

    Delete trivia directory

--- trivia/trivia/site/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.template import Template, Context
-from django.shortcuts import render
-
-def triviaPlantilla(request):
-     return render(request, 'home.html')
